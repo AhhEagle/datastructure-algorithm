59aab783023b3414f09ca0f888423c8c70f2076a
AhhEagle <dim2though@gmail.com>
2021-06-10 08:50:02 +0100

    Implement O(n)

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ console.log(OddorEven(20));
 console.log(OddorEven(30001));
 
 //O(n)
+//finding the largest item in an unsorted array
+function max(n){
+    let max = 0
+    if(!Array.isArray(n)){
+        return 'Input must be an array'
+    }
+    for(let i = 0; i < n.length; i++){
+        if(max === undefined || max < n[i])
+        max = n[i];
+    }
+    return max
+}
+console.log(max([3,2,1]));
+console.log(max([10,50,30,80,100,90]));
