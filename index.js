1eefd086806f2f06b05752efd03c6b9fc0f2d3d5
AhhEagle <dim2though@gmail.com>
2021-06-10 08:31:45 +0100

    Implement O(1)

---
+++ index.js
@@ -0,0 +1,14 @@
+//O(1)
+function multiply(j){
+    return j * 5;
+}
+console.log(multiply(10));
+console.log(multiply(2));
+
+function OddorEven(j){
+  return j % 2 ? 'Odd' : 'Even';
+}
+console.log(OddorEven(20));
+console.log(OddorEven(30001));
+
+//O(n)
