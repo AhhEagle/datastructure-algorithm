77533f2e493c7267fa60b44ffc74710b1329af96
AhhEagle <dim2though@gmail.com>
2021-06-29 09:49:33 +0100

    Implement stack

--- stack.js
+++ stack.js
@@ -1,1 +1,20 @@
 // checking if a word is a palindrome or not
+function palin(str){
+    let letters = []; // the stack
+   let reversed = ""; // reversed word
+
+for(let i = 0; i <str.length; i++){
+    letters.push(str[i]);
+}
+for(let i = 0; i<str.length; i++){
+    reversed += letters.pop()
+}
+if (reversed == str){
+    return `${str} is a palindrome`;
+} else{
+    return `${str} is not a palindrome`;
+ 
+}
+}
+
+console.log(palin('dimeji'))
