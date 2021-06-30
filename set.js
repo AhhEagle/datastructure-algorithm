425f3646aac062f97508e870108a34e34b15202d
AhhEagle <dim2though@gmail.com>
2021-06-30 08:58:44 +0100

    Implement add method to a set

---
+++ set.js
@@ -0,0 +1,19 @@
+function mySet(){
+let collection = [];
+this.has =  function(element){
+    return(collection.indexOf(element) !== -1);
+};
+this.values = function(){
+    return collection;
+};
+
+this.add= function(element){
+    if(!this.has(element)){
+        collection.push(element);
+        return true;
+    }
+    return false;
+};
+
+this.remove = function(element)
+}
