23c0d4a3d8d75d0abde67deed494837a6cbff1af
AhhEagle <dim2though@gmail.com>
2021-07-01 09:44:36 +0100

    Implement Queue methods

---
+++ queue.js
@@ -0,0 +1,18 @@
+function Queue(){
+    collection =  [];
+    this.print = () => console.log(collection);
+    this.enqueue= (element) => console.log(collection.push(element));
+    this.dequeue= () => console.log(collection.shift());
+    this.front=()=>{return collection[0]};
+    this.size=()=>{ return collection.length};
+    this.isEmpty= ()=>{return collection.length === 0};
+}
+
+let q = new Queue();
+q.enqueue('a');
+q.enqueue('b');
+q.enqueue('c');
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
