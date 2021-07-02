8f12312e9674831fa18f5d180bcfbe56d4724101
AhhEagle <dim2though@gmail.com>
2021-07-02 09:26:18 +0100

    Implement bst node

--- bst.js
+++ bst.js
@@ -11,3 +11,27 @@ class Node {
         this.right = right;
     }
 }
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+    add(data){
+        const node = this.root;
+        if(node == null){
+            this.root = new Node(data);
+            return;
+        } else{
+            const searchTree = (node) =>{
+                if(data < node.data){
+                    if(node.left === null){
+                        node.left = new Node(data);
+                        return;
+                    } else if(node.left !==null){
+                        return searchTree(node.left);
+                    }
+                }
+            }
+        }
+    }
+}
