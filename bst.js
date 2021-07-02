a9a9cb32b19c5be66eb5a41e2672236972a94a18
AhhEagle <dim2though@gmail.com>
2021-07-02 08:31:24 +0100

    bst setup

---
+++ bst.js
@@ -0,0 +1,5 @@
+//Binary Search Tree
+//Binary tree can only have two branches for every node. 
+//BST are ordered.
+//Each subtree is less than or equal to the parent node 
+//and each subtree is greater than or equal to the parent tree
