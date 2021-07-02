599f0a581b01dacb0fdec055925a47b5ab67be6b
AhhEagle <dim2though@gmail.com>
2021-07-02 09:20:43 +0100

    Implment bst node

--- bst.js
+++ bst.js
@@ -3,3 +3,11 @@
 //BST are ordered.
 //Each subtree is less than or equal to the parent node 
 //and each subtree is greater than or equal to the parent tree
+
+class Node {
+    constructor(data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
