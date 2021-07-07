7add9f4f6cde2497125c879a2ae00ddd1f73167a
AhhEagle <dim2though@gmail.com>
2021-07-07 09:22:03 +0100

    Implement find data bst

--- bst.js
+++ bst.js
@@ -59,4 +59,20 @@ class BST{
         }
         return current.data;
     }
+    find(data){
+        let current = this.root;
+        while(current.data !== data){
+            if(data < current.data){
+                current = current.left; 
+            } else {
+                current = current.right
+            }
+            if (current === null){
+                return null;
+            }
+
+        }
+        return current;
+    }
+    
 }
