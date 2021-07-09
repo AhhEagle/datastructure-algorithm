421cf06ff4ebb480ff053c526f4f7a040583079f
AhhEagle <dim2though@gmail.com>
2021-07-09 10:41:56 +0100

    Modify Code

--- index.js
+++ index.js
@@ -226,9 +226,9 @@ function getPermutation(string, prefix=''){
 //     return true;
 //   }
 
-function mergeTwo(arr1, arr2) {
+/* function mergeTwo(arr1, arr2) {
     let result = [...arr1, ...arr2];
     return result.sort((a,b) => a-b);
-  }
+  }*/
   
   //console.log(mergeTwo([1,3,5,7], [2,4,6]));
