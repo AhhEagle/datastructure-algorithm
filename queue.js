f16de5a66ed841bf29c9fe6ebf3666f031522c09
AhhEagle <dim2though@gmail.com>
2021-08-19 10:39:38 +0100

    Modify priorityQueue

--- queue.js
+++ queue.js
@@ -8,30 +8,7 @@ function Queue(){
     this.isEmpty= ()=>{return collection.length === 0};
 }
 
-function PriorityQueue(){
-    let collection = [];
-    this.printCollection =  function(){
-        console.log(collection)
-    };
-    this.enqueue = function(element){
-        if(this.isEmpty()){
-            collection.push(element);
-        } else {
-            let added = false;
-            for(let i=0; i<collection.length; i++){
-                if(element[1] < collection[i][1]){ // to check priorities
-                    collection.splice(i,0,element);
-                    added = true;
-                    break;
-                }
-            }
-            if(!added){
-                collection.push(element);
-            }
-        }
-    };
-    
-}
+
 
 let q = new Queue();
 q.enqueue('a');
