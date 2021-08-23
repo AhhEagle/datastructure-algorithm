c9229ca67318f4f9a409ddc3dafb2f5ccf17b9aa
AhhEagle <dim2though@gmail.com>
2021-08-23 15:05:27 +0100

    Implement bst isBalanced

--- bst.js
+++ bst.js
@@ -126,6 +126,9 @@ class BST{
         }
         this.root = removeNode(this.root, data);
     }
+    isBalanced(){
+        return(this.findMinHeight() >= this.findMaxHeight() - 1);
+    }
    findMinHeight(node = this.root){
         if(node == null){
             return -1;
