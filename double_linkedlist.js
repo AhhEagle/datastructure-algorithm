b3a689cfe7bd7a38a13dacdd7862ae7a8e802ae8
AhhEagle <dim2though@gmail.com>
2021-10-20 17:34:42 +0100

    Implement addFirst double linked

--- double_linkedlist.js
+++ double_linkedlist.js
@@ -6,10 +6,30 @@ class Node {
     }
 }
 
-class SingleLinked{
+class DoubleLinked{
    constructor(){
         this.head;
         this.tail;
         this.size = 0;
    }
+   addFirst(data){
+       let node = new Node(data);
+       if(this.head == null){
+           this.head = this.tail = node;
+       } else{
+           node.next = this.head;
+           this.head.prev = node ;
+           this.head = node;
+       }
+
+       this.size++;
+   }
 }
+
+const double =  new DoubleLinked()
+double.addFirst(5);
+double.addFirst(4);
+double.addFirst(3);
+double.addFirst(2);
+console.log(double);
+
