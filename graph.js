ca811d203d46d48a3f76b6b9004b62b20f0a1f3d
AhhEagle <dim2though@gmail.com>
2021-11-18 13:05:46 +0100

    addEdge on a graph

--- graph.js
+++ graph.js
@@ -7,13 +7,25 @@ class Node {
 class graph{
     constructor(){
          this.nodes = {};
+         this.adjacency = {};
+         this.list = [];
     }
     add(label){
         let node =  new Node(label)
         this.nodes[label] = node;
+        this.adjacency[node] = this.list;
     }
 
     addEdge(from, to){
         let fromNode = this.nodes[from];
+        if(fromNode == null){
+            throw new Error("IllegalArgument");
+        }
+        let toNode = this.nodes[to];
+        if(toNode == null){
+            throw new Error("IllegalArgument");
+        }
+    this.adjacency[fromNode] = this.list.push(toNode);
     }
+
 }
