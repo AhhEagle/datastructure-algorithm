aa3b08488247401a71291c3017eb4c690818ad7c
unknown <aliyu.olateju@gmail.com>
2022-03-16 08:20:18 +0000

    Implement Adjancent list method

--- graphList.js
+++ graphList.js
@@ -3,4 +3,21 @@ class Node{
         this.label = label;
         this.list = [];
     }
+
+    addAdjancent(node){
+        this.list.push(node);
+    }
+    
+    getAdjancents(){
+        return this.list;
+    }
+    
+    removeAdjancent(node){
+        const index = this.list.indexOf(node);
+        return this.list.splice(index, 1)
+    }
+
+    isAdjacent(node){
+        return this.list.indexOf(node) > -1;
+    }
 }
