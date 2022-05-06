c4656494aefb505cd05e01dd112456db468e92e5
AhhEagle <aliyu.olateju@gmail.com>
2022-05-06 11:52:39 +0100

    Implement insertAfter for double linked list

--- double_linkedlist.js
+++ double_linkedlist.js
@@ -29,13 +29,32 @@ class DoubleLinked{
        if(this.head == null){
            this.head = this.tail = node;
        } else{
+            node.prev = this.tail;
           this.tail.next = node;
-          node.prev = this.tail;
           this.tail = node;
        }
 
        this.size++;
    }
+
+   insertAfter(data, nodeAfter) {
+    let current = this.head;
+    while (current) {
+      if (current.data === nodeAfter) {
+        const node = new Node(data);
+        if (current === this.tail) {
+          this.addLast(data);
+        } else {
+          current.next.previous = node;
+          node.previous = current; 
+          node.next = current.next;
+          current.next = node;
+          this.size++;
+        }
+      }
+      current = current.next;
+    }
+  }
 
    indexOf(data){
        let index = 0;
