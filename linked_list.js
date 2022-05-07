f1493e194ad4005e672d47f9340b103bd5faf270
AhhEagle <aliyu.olateju@gmail.com>
2022-05-07 12:48:14 +0100

    Modify LinkedList

--- linked_list.js
+++ linked_list.js
@@ -221,16 +221,18 @@ class SingleLinked{
         return head
     }
 
-    hasCycle(){
+    hasCycle(linkedlist){
+        let cycle_length = 0;
         let fast = this.head, slow = this.head;
         while(fast !== null && fast.next !== null){
             fast= fast.next.next;
             slow = slow.next;
             if(slow === fast){
-                return this.calculate_cycle_length(slow)
+             cycle_length = this.calculate_cycle_length(slow);
+             break;
             }
         }
-        return 0;
+        return this.find_cycle_start(linkedlist, cycle_length);
     }
      calculate_cycle_length(slow) {
         let current = slow,
@@ -243,6 +245,23 @@ class SingleLinked{
           }
         }
         return cycleLength;
+      }
+
+       find_cycle_start(head, cycle_length) {
+        let pointer1 = head,
+          pointer2 = head;
+        // move pointer2 ahead 'cycle_length' nodes
+        while (cycle_length > 0) {
+          pointer2 = pointer2.next;
+          cycle_length -= 1;
+        }
+        // increment both pointers until they meet at the start of the cycle
+        while (pointer1 !== pointer2) {
+          pointer1 = pointer1.next;
+          pointer2 = pointer2.next;
+        }
+      
+        return pointer1;
       }
 
       
