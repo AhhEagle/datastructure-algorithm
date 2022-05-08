1c9d7118a2137341886a965ec3886d2ce405e08a
AhhEagle <aliyu.olateju@gmail.com>
2022-05-08 12:18:36 +0100

    Implement count vowels

--- stack.js
+++ stack.js
@@ -92,3 +92,26 @@ var decodeString = function(s) {
 };
 //console.log(decodeString("3[a]"))
 
+wordWrap = function(str, cols) {
+    var formatedString = '',
+       wordsArray = [];
+
+
+   wordsArray = str.split(' ');
+
+   formatedString = wordsArray[0];
+
+   for (var i = 1; i < wordsArray.length; i++) {
+       if (wordsArray[i].length > cols) {
+           formatedString += '\n' + wordsArray[i];
+       } else {
+           if (formatedString.length + wordsArray[i].length > cols) {
+               formatedString += '\n' + wordsArray[i];
+           } else {
+               formatedString += ' ' + wordsArray[i];
+           }
+       }
+   }
+
+   return formatedString
+}
