7b8cb8fe8b6090622e69360b3c17bcb6833d12a8
AhhEagle <aliyu.olateju@gmail.com>
2022-05-09 10:51:20 +0100

    Impelement Capitalize first character in a sentence

--- string_manipulate.js
+++ string_manipulate.js
@@ -83,5 +83,11 @@ function mostRepeated(str){
     return maxChar;
 }
 
-console.log(mostRepeated(""))
+function capitalizeFirst(sentence){
+const words = sentence.split(" ");
+return words.map((word) => { 
+    return word[0].toUpperCase() + word.substring(1); 
+}).join(" ");
+}
+
 
