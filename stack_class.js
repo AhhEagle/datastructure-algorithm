f46cdf50d9c606328e7ad4a5974fd1def6fad091
AhhEagle <aliyu.olateju@gmail.com>
2022-05-15 17:48:17 +0100

    Implement another stack class

---
+++ stack_class.js
@@ -0,0 +1,47 @@
+class Stack{
+    constructor(){
+        this.items = [];
+        this.top = null;
+        this.count = 0;
+    }
+
+    peek(){
+        if(this.count == 0)
+            return null;
+        
+        return this.top;
+    }
+
+    isEmpty(){
+        return this.count == 0 ? true : false; 
+    }
+    size(){
+        return this.count
+    }
+    push(element){
+        this.items.push(element);
+        this.top = element;
+        this.count++;
+    }
+    pop(){
+        if(this.count == 0){
+            return null;
+        } else {
+            this.count = this.count - 1 
+            console.log(this.count)
+            this.top = this.items[this.count - 1];
+            return this.items.pop();
+        }
+    }
+}
+
+let testStack =  new Stack();
+// console.log(testStack.push(5));
+// console.log(testStack.push(7));
+// console.log(testStack.push(8));
+// console.log(testStack.peek());
+// console.log("this is empty", testStack.isEmpty());
+// console.log(testStack.size());
+// console.log("This is pop", testStack.pop());
+// console.log(testStack.peek());
+// console.log(testStack.size());
