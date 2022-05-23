f1b1db5420f97a30b40817452f1bd055066efeac
AhhEagle <aliyu.olateju@gmail.com>
2022-05-23 17:22:19 +0100

    Implement remove and dft

--- bstTwo.js
+++ bstTwo.js
@@ -35,6 +35,82 @@ class BstTwo{
             }
         }
     }
+
+    findMin(){
+        let current = this.root;
+        while(current.left !== null){
+            current = current.left;
+        }
+        return current.data
+    }
+
+    remove(data){
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, data){
+        if(!node){
+            return null
+        }
+
+        if(data < node.data){
+            node.left = this.removeNode(node.left, data);
+            return node
+        } else if (data > node.data){
+            node.right = this.removeNode(node.right, data)
+            return node
+        } else {
+            if(!node.left && !node.right){
+                node = null;
+                return node;
+            }
+
+            if(!node.left){
+                node = node.right;
+                return node;
+            }
+
+            if(!node.right){
+                node = node.left;
+                return node;
+            }
+            let min = this.findMin(node.right);
+            node.data = min.data;
+            node.right = this.removeNode(node.right, min.data);
+            return node;
+        }
+    }
+
+    inOrder(node){
+        if(node){
+            this.inOrder(node.left);
+            console.log(node.data);
+            this.inOrder(node.right);
+        } else {
+            return null;
+        }
+    }
+
+    preOrder(node){
+        if(node){
+            console.log(node.data);
+            this.preOrder(node.left);
+            this.preOrder(node.right);
+        } else {
+            return null;
+        }
+    }
+
+    postOrder(node){
+        if(node){
+            this.postOrder(node.left);
+            this.postOrder(node.right);
+            console.log(node.data);
+        } else {
+            return null;
+        }
+    }
+
     
 }
 
