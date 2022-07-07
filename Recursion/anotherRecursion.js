4ffc26e364e3a080f18c92603fb42a205a517eca
AhhEagle <aliyu.olateju@gmail.com>
2022-07-07 15:32:40 +0100

    Implement searchFirstIndex recursively

--- Recursion/anotherRecursion.js
+++ Recursion/anotherRecursion.js
@@ -32,3 +32,17 @@ function isVowel(character) {
       return findSquare(testVariable - 1) + (2 * testVariable) - 1;
     }
   }
+
+  function searchFirstIndex(arr, testVariable, currentIndex) { 
+      // returns the first occurrence of testVariable
+    // Base case1
+    if (arr.length == currentIndex) {
+      return -1;
+    }
+    // Base case2  
+    if (arr[currentIndex] == testVariable) {
+      return currentIndex;
+    }
+    // Recursive case
+    return firstIndex(arr, testVariable, currentIndex + 1);
+  }
