5fd2205a19d4fb8204de6fd87a4aaa31fac1b1bf
AhhEagle <aliyu.olateju@gmail.com>
2022-07-07 15:20:17 +0100

    Implement findsquare recursively

--- Recursion/anotherRecursion.js
+++ Recursion/anotherRecursion.js
@@ -19,3 +19,16 @@ function isVowel(character) {
     // Recursive case
     return countVowels(string, stringLength - 1) + isVowel(string[stringLength - 1]); 
   }
+
+  function findSquare(testVariable) {
+      //testVariable * testVariable
+    // Base case
+    if (testVariable == 0) {
+      return 0;
+    }
+  
+    // Recursive case
+    else {
+      return findSquare(testVariable - 1) + (2 * testVariable) - 1;
+    }
+  }
