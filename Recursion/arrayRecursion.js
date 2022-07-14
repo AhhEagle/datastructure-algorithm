1b5f254c7f53c94ef0391467def0cf0f7f847f7b
AhhEagle <aliyu.olateju@gmail.com>
2022-07-14 13:35:23 +0100

    Implement sort array recursively

--- Recursion/arrayRecursion.js
+++ Recursion/arrayRecursion.js
@@ -43,3 +43,26 @@ function count(myArray, key) {
 	// Compute sum 
 	return (testVariable[currentIndex] + average(testVariable, currentIndex + 1)) 
 }
+
+function sort(testVariable, length) {
+	// Base case
+	if (length <= 1) {
+		return
+  }
+	
+	// Recursive case
+	// Call sort() again for first n - 1 elements
+	sort(testVariable, length - 1)
+
+	// Insert last element at its correct position in sorted array
+	var lastElement = testVariable[length - 1] // fetch the last element
+	var temp = length - 2 // start finding its correct location from one element before it
+	
+	// Move elements of testVariable[0..i-1], that are greater than key, to one position ahead of their current position 
+	while (temp >= 0 && testVariable[temp] > lastElement) {
+		testVariable[temp + 1] = testVariable[temp]
+		temp = temp - 1
+  }
+
+	testVariable[temp + 1] = lastElement // place the element in its correct position
+}
