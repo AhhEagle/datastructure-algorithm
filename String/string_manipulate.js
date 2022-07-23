ba22c0bbf9a93b8fda55f2b2bec72ee34b91e910
AhhEagle <aliyu.olateju@gmail.com>
2022-07-23 14:29:15 +0100

    Implement Array Subset

--- String/string_manipulate.js
+++ String/string_manipulate.js
@@ -125,4 +125,20 @@ function wordSegment(str, dictionary){
       return false;
 }
 
+function stringRotation(str1, str2) {
+    if(str1.length !== str2.length) {
+        return false;
+    }
+  
+    for(let i = 0; i < str1.length; i++) {
+        const rotation = str1.slice(i, str1.length) + str1.slice(0, i);
+        
+        if(rotation === str2) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
 
