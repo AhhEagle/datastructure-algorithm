f495ad03a018a12dcec97831c46ecfd6cdd7c0f8
AhhEagle <aliyu.olateju@gmail.com>
2022-07-26 13:57:08 +0100

    Implement rotate matrix

--- Array/new_array.js
+++ Array/new_array.js
@@ -84,3 +84,24 @@ function allAnagrams(strings) {
     
     return true;
 }
+
+//Rotate array matrix
+function rotateClockwise(matrix) {
+    const newMatrix = matrix[0].map(() => []);
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            newMatrix[j][matrix.length - 1 - i] = matrix[i][j];
+        }
+    }
+
+    return newMatrix;
+}
+
+function rotate180(matrix) {
+    return rotateClockwise(rotateClockwise(matrix));
+}
+
+function rotateCounterClockwise(matrix) {
+    return rotateClockwise(rotate180(matrix));
+}
