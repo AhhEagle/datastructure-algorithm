b9896f5360756ae25820e31d78a88a745eaac200
AhhEagle <aliyu.olateju@gmail.com>
2022-08-07 15:26:02 +0100

    Implement cyclic sort array

--- Array/rotate_array_adv.js
+++ Array/rotate_array_adv.js
@@ -60,3 +60,16 @@ function rotateClockwise(mat) {
     return binarySearch(nums, 0, nums.length - 1, target);
   };
   
+
+  function cyclic_sort(nums) {
+    let i = 0;
+    while (i < nums.length) {
+      const j = nums[i] - 1;
+      if (nums[i] !== nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+      } else {
+        i += 1;
+      }
+    }
+    return nums;
+  }
