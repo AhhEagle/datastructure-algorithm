e66346d44c3817bcf78707804a9c59b5236b8939
AhhEagle <aliyu.olateju@gmail.com>
2023-01-03 15:30:43 +0000

    Implement merge two sorted arrays

--- Array/array_basic.js
+++ Array/array_basic.js
@@ -13,4 +13,28 @@ function removeEven(arr){
 function anotherRemoveEven(arr){
    return arr.filter((v => (v % 2 != 0)))
 }
-console.log(anotherRemoveEven([1,2,4,5,10,6,3]));
+
+function mergeSortedArray(arr, arr1){
+    let merged = [];
+    let i = 0, j = 0;
+    while((i < arr.length) && (j < arr1.length)){
+        if (arr[i] < arr1[j]){
+            merged.push(arr[i]);
+            i++;
+        } else {
+            merged.push(arr1[j]);
+            j++
+        }
+    }
+    
+    if(i <= (arr.length - 1)){
+        arr.splice(0, i)
+        merged = merged.concat(arr);
+    } else if (j <= (arr1.length - 1)){
+        arr1.splice(0, j);
+        merged = merged.concat(arr1);
+    }
+    return merged 
+}
+console.log(mergeSortedArray([1,3,4,5], [2,6,7,8]))
+
