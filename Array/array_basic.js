527ccaef15895817f845bef7f05397f7e0d8442b
AhhEagle <aliyu.olateju@gmail.com>
2023-01-06 22:29:19 +0000

    Implement find the product of an array

--- Array/array_basic.js
+++ Array/array_basic.js
@@ -71,4 +71,21 @@ function binarySearch(arr, item){
 
 }
 
+function findProduct(arr) {
+    var temp = 1,
+        product = [];
+    for (let i = 0; i < arr.length ; i++) {
+        product[i]  = temp;
+        temp = temp * arr[i];
+    }
+
+    temp = 1;
+    for (let i = arr.length - 1; i > -1; i--) {
+        product[i] *= temp;
+        temp *= arr[i];
+    }
+
+    return product
+}
+
 
