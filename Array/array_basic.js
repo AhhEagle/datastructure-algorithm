bdfd250f7ab558f5e1941913832516cdf04d7652
AhhEagle <aliyu.olateju@gmail.com>
2023-01-09 23:49:17 +0000

    Implement rearrange positive number in an array

--- Array/array_basic.js
+++ Array/array_basic.js
@@ -126,3 +126,18 @@ function findFirstUnique(arr){
     return (arr.splice(arr.length - n)).concat(arr.splice(0, arr.length))
 }
 
+function reArrange(arr) {
+    let leftMostPosEle = 0,
+        tmp;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            if (i != leftMostPosEle) {
+                tmp = arr[i];
+                arr[i] = arr[leftMostPosEle];
+                arr[leftMostPosEle] = tmp;
+            }
+            leftMostPosEle += 1
+        }
+    }
+    return arr;
+}
