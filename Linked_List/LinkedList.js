8ca0dde46a36387535c0a80df576c70b8cccbeb5
AhhEagle <aliyu.olateju@gmail.com>
2023-01-15 20:11:22 +0000

    Search singly linked list

--- Linked_List/LinkedList.js
+++ Linked_List/LinkedList.js
@@ -56,3 +56,25 @@ module.exports = class LinkedList {
   }
 
 }
+
+"use strict";
+const LinkedList = require('./LinkedList.js');
+const Node = require('./Node.js');
+//Access HeadNode => list.getHead()
+//Check if list is empty => list.isEmpty()  
+//Node class  { data ; Node nextElement;}
+
+//Searches a value in the given list.
+LinkedList.prototype.search = function(value) {
+  //Start from the first element
+  let currentNode = this.head;
+
+  //Traverse the list until you find the value or reach the end
+  while (currentNode != null) {
+    if (currentNode.data == value) {
+      return true; //value found
+    }
+    currentNode = currentNode.nextElement
+  }
+  return false; //value not found
+}
