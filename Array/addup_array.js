1ba5bf996df2f5c76069955b7f5a98886fc958eb
AhhEagle <aliyu.olateju@gmail.com>
2023-02-09 15:03:26 +0000

    Implement another sum up

--- Array/addup_array.js
+++ Array/addup_array.js
@@ -10,4 +10,40 @@ function sumUp(arr, val){
 
 }
 
-console.log(sumUp([2,4,6,8], 10))
+function binarySearch(arr, item){
+    let first = 0;
+    let mid;
+    let last = arr.length - 1;
+    let arrayIndex = -1
+    let seen = false;
+    while ((first <= last) && !seen){
+        mid = Math.floor((first + last)/2);
+        if( arr[mid] == item){
+            arrayIndex = mid
+            seen = true
+        }
+        else{
+            if(item < arr[mid])
+                last = mid - 1
+            else
+                first = mid + 1
+        }
+    }
+    if (seen)
+    return arrayIndex
+else
+    return false
+}
+function anotherSumUp(arr, val){
+    arr.sort((a,b)=>{return a-b});
+    let index;
+    for(let i =0; i<arr.length; i++){
+        index =  binarySearch(arr, val-arr[i])
+        if(index != false && i!=index){
+            return [arr[i], val-arr[i]]
+        }
+    }
+    return false;
+
+}
+console.log(anotherSumUp([2,4,6,8], 10))
