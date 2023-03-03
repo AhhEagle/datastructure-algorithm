ea4ead0ed62debafce4a1b27e11871b5d60e8240
AhhEagle <aliyu.olateju@gmail.com>
2023-03-03 22:51:18 +0000

    Modify code

--- Recursion/anotherRecursion.js
+++ Recursion/anotherRecursion.js
@@ -11,12 +11,9 @@ function isVowel(character) {
   }
   
   function countVowels(string, stringLength) { 
-      // Base case
     if (stringLength == 1) {
         return Number(isVowel(string[0])); 
     }
-  
-    // Recursive case
     return countVowels(string, stringLength - 1) + isVowel(string[stringLength - 1]); 
   }
 
