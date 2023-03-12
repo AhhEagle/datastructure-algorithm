f2a21a7c62c89275c13bd4407e987a34ffb16997
AhhEagle <aliyu.olateju@gmail.com>
2023-03-12 10:40:53 +0000

    Implement longest best sub array

--- Array/test.js
+++ Array/test.js
@@ -138,5 +138,19 @@ function numSubarrayProductLessThanK(arr, k){
     } 
     return ans 
 }
+function longestBestArr(nums, k){
+        let curr = 0;
+        for (let i = 0; i < k; i++) {
+            curr += nums[i];
+        }
+        
+        let ans = curr;
+        for (let i = k; i < nums.length; i++) {
+            curr += nums[i] - nums[i - k];
+            ans = Math.max(ans, curr);
+        }
+        
+        return ans;
+}
 
 
