62c71818009c265a09a6a34115462105129d8c7d
AhhEagle <aliyu.olateju@gmail.com>
2023-03-18 11:48:53 +0000

    Generate binary nos

---
+++ Queue/another_queue.js
@@ -0,0 +1,19 @@
+//generate binary numbers from 1 till n
+function findBin(n) {
+    let result = [];
+    let myQueue = new Queue();
+    var s1, s2;
+    myQueue.enqueue("1");
+    for (var i = 0; i < n; i++) {
+
+        result.push(myQueue.dequeue());
+        s1 = result[i] + "0";
+        s2 = result[i] + "1";
+
+        myQueue.enqueue(s1);
+        myQueue.enqueue(s2);
+
+    }
+
+    return result;
+}
