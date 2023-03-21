97897798cf5508f3d0ea5c61e68a45a9c8073f2d
AhhEagle <aliyu.olateju@gmail.com>
2023-03-21 09:53:11 +0000

    Implement another graph

---
+++ graph/graph_challenge/graphlist.js
@@ -0,0 +1,16 @@
+class Graph{
+    constructor(vertices){
+      //Total number of vertices in the graph
+      this.vertices=vertices;
+      this.list=[];
+      for(i=0; i<vertices.length; i++){
+        let temp=new LinkedList();
+        this.list.push(temp);
+      }
+    }
+
+    addEdge(source, destination) {
+        if (source < this.vertices && destination < this.vertices)
+         this.list[source].insertAtHead(destination);
+      }
+  }
