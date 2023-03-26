3e41208e6111506edde24b9c46deda424a535af3
AhhEagle <aliyu.olateju@gmail.com>
2023-03-26 21:22:56 +0100

    Depth first transversal

--- graph/graph_challenge/graphlist.js
+++ graph/graph_challenge/graphlist.js
@@ -18,6 +18,23 @@ class Graph{
       }
 
       strGraph() {
+        //breadth First
+        let str = "";
+        var i;
+        for (i = 0; i < this.list.length; i++) {
+          str = str + "|" + String(i) + "| => ";
+          let temp = this.list[i].getHead();
+          while (temp != null) {
+            str += ("[" + String(temp.data) + "] -> ");
+            temp = temp.nextElement;
+          }
+          str += "null ";
+        }
+        return str;
+      }
+
+      dfGraph() {
+        //depth first 
         let str = "";
         var i;
         for (i = 0; i < this.list.length; i++) {
