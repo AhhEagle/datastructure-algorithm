4dd03bdd364f40f30f387b9859e07e7499f55994
AhhEagle <aliyu.olateju@gmail.com>
2023-04-01 21:59:52 +0100

    Shortest path between two vertices

--- graph/graph_challenge/graphlist.js
+++ graph/graph_challenge/graphlist.js
@@ -374,5 +374,52 @@ checkCycle(g, node, visited, parent) {
   return false;
 }
 
+findMin(g, a, b){
+  if (a==b){
+    return 0;
+  }
+
+  let num_of_vertices = g.vertices;
+  //A list to hold the history of visited nodes (by default all false)
+  //Make a node visited whenever you enqueue it into queue
+  let visited = [];
+  for(var i=0; i<num_of_vertices; i++){
+    visited[i]=false;
+  }
+ 
+  //For keeping track of distance of current_node from source
+  let distance = [];
+  for(var j=0; j<num_of_vertices; j++){
+    distance[j]= 0;
+  }
+  
+  //Create Queue for Breadth First Traversal and enqueue source in it
+  let queue = new Queue(num_of_vertices);
+  queue.enqueue(a);
+  visited[a] = true;
+  //Traverse while queue is not empty
+  while (queue.isEmpty()==false){
+    //Dequeue a vertex/node from queue and add it to result
+    let current_node = queue.dequeue();
+    //Get adjacent vertices to the current_node from the list,
+    //and if they are not already visited then enqueue them in the Queue
+    //and also update their distance from `a` by adding 1 in current_nodes's distance
+    let temp = g.list[current_node].head;
+    while (temp != null){
+      if (visited[temp.data]==false){
+        queue.enqueue(temp.data);
+        visited[current_node] = true
+        	distance[temp.data] = distance[current_node] + 1;
+        }
+        if(temp.data== b){
+        	 return distance[b];
+        }
+      temp = temp.nextElement;
+  }
+}
+  //end of while
+return -1;
+}
+
   
 }
