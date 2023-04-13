c5bb5991085c512bc42e9a6d474b0f9b94fa3563
AhhEagle <aliyu.olateju@gmail.com>
2023-04-13 23:26:06 +0100

    Implement hash table

---
+++ Hash/hashTable.js
@@ -0,0 +1,30 @@
+"use strict";
+const HashEntry = require('./hashEntry.js');
+
+module.exports = class HashTable {
+  //Constructor
+  constructor() {
+    //Size of the HashTable
+    this.slots = 10;
+    //Current entries in the table
+    //Used while resizing the table when half of the table gets filled
+    this.size = 0;
+    //List of HashEntry objects (by deafult all null)
+    this.bucket = [];
+    for (var i = 0; i < this.slots; i++) {
+      this.bucket[i] = null;
+    }
+  }
+  //Helper Functions  
+  get_size() {
+    return this.size;
+  }
+  //Hash Function
+  getIndex(key) {
+    let index = key % this.slots;
+    return index;
+  }
+  isEmpty() {
+    return this.get_size() == 0;
+  }
+}
