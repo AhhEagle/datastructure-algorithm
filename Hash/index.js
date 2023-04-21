46b79e80e1b3c33baeeb231e2d3108408e07bc96
AhhEagle <aliyu.olateju@gmail.com>
2023-04-21 19:25:26 +0100

    remove dup in a linked list

--- Hash/index.js
+++ Hash/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const HashEntry = require('./hashEntry.js');
 const HashTable = require('./hashTable.js');
+const LinkedList = require('./LinkedList.js');
 
 let threshold = 0.6
 HashTable.prototype.resize = function (){
@@ -241,3 +242,42 @@ function findFirstUnique(arr) {
 
 	return null;
 }
+
+function removeDuplicates(list) {
+  let currentNode = list.getHead();
+  let prevNode = list.getHead();
+  //To store values of nodes which we already visited
+  let visitedNodes = new HashTable();
+  //If List is not empty and there is more than 1 element in List
+  if (!list.isEmpty() && currentNode.nextElement != null) {
+    while (currentNode != null) {
+      let value = currentNode.data;
+      if (visitedNodes.search(value) != null) {
+        //currentNode is a duplicate as its value is already in the HashSet
+        //so connect prevNode with currentNode's next element to remove it
+        prevNode.nextElement = currentNode.nextElement;
+        currentNode = currentNode.nextElement;
+        continue;
+      }
+      visitedNodes.insert(currentNode.data, 1); //Visiting currentNode for first time
+      prevNode = currentNode;
+      currentNode = currentNode.nextElement;
+    }
+  }
+  return list;
+}
+
+let list = new LinkedList();
+list.insertAtHead(7);
+list.insertAtHead(7);
+list.insertAtHead(7);
+list.insertAtHead(22);
+list.insertAtHead(14);
+list.insertAtHead(21);
+list.insertAtHead(14);
+list.insertAtHead(7);
+
+
+list.printList();
+removeDuplicates(list);
+list.printList();
