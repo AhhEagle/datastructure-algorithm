3b49621e45b4b9470ebf808068807e8e1b7b3be2
AhhEagle <aliyu.olateju@gmail.com>
2023-04-28 20:50:15 +0100

    Code clean up

--- Hash/hashEntry.js
+++ Hash/hashEntry.js
@@ -2,9 +2,7 @@
 module.exports = class HashEntry {
   constructor(key, data) {
     this.key = key;
-    // data to be stored
     this.value = data;
-    // reference to new entry
     this.next = null;
   }
 }
