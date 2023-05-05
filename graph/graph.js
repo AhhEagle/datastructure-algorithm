a9c5ffc07833bd499a20fd06cdc588e0acc9ebdc
AhhEagle <aliyu.olateju@gmail.com>
2023-05-05 23:51:22 +0100

    modify:graph

--- graph/graph.js
+++ graph/graph.js
@@ -1,48 +1,46 @@
 class Node {
-    constructor(label = null){
-        this.label = label;
-    }
+  constructor(label = null) {
+    this.label = label;
+  }
 }
 
-class graph{
-    constructor(){
-         this.nodes = {};
-         this.adjacencyList = {};
-    }
-    add(label){
-        let node =  new Node(label)
-        this.nodes[label] = node;
-        this.adjacencyList[node] =  [];
-    }
+class graph {
+  constructor() {
+    this.nodes = {};
+    this.adjacencyList = {};
+  }
+  add(label) {
+    let node = new Node(label);
+    this.nodes[label] = node;
+    this.adjacencyList[node] = [];
+  }
 
-    addEdge(from, to){
-        let fromNode = this.nodes[from];
-        if(fromNode == null){
-            throw new Error("IllegalArgument");
-        }
-        let toNode = this.nodes[to];
-        if(toNode == null){
-            throw new Error("IllegalArgument");
-        }
+  addEdge(from, to) {
+    let fromNode = this.nodes[from];
+    if (fromNode == null) {
+      throw new Error("IllegalArgument");
+    }
+    let toNode = this.nodes[to];
+    if (toNode == null) {
+      throw new Error("IllegalArgument");
+    }
     this.adjacencyList[fromNode].push(toNode);
+  }
+  print() {
+    for (let x in Object.keys(this.adjacencyList)) {
+      var targets = this.adjacencyList[x];
+      if (targets != null) {
+        console.log(x + " is connected to " + targets);
+      }
     }
-    print(){
-        for(let x in Object.keys(this.adjacencyList)){
-            var targets = this.adjacencyList[x];
-            if(targets != null){
-                console.log(x + " is connected to " + targets)
-            }
-        }
-
-    }
-
+  }
 }
-const graphtest =  new graph();
+const graphtest = new graph();
 graphtest.add("A");
 graphtest.add("B");
 graphtest.add("C");
 graphtest.addEdge("A", "B");
 graphtest.addEdge("A", "C");
-graphtest.addEdge("B", "C")
+graphtest.addEdge("B", "C");
 graphtest.print();
 console.log(graphtest);
