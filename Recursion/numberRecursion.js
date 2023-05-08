db60d57c696fb14b84854f3aed2ac9a2f1c5adb7
AhhEagle <aliyu.olateju@gmail.com>
2023-05-08 18:20:46 +0100

    modify:update number recursion

--- Recursion/numberRecursion.js
+++ Recursion/numberRecursion.js
@@ -1,30 +1,29 @@
-function exponential(base, expo){
-    if (expo === 0){
-        return 1;
-    } else {
-        return base * exponential(base, expo - 1)
-    }
-
+function exponential(base, expo) {
+  if (expo === 0) {
+    return 1;
+  } else {
+    return base * exponential(base, expo - 1);
+  }
 }
 
-function summation(testVariable){
-    if(testVariable == 1){
-        return 1
-    } else {
-        return testVariable + summation(testVariable - 1)
-    }
+function summation(testVariable) {
+  if (testVariable == 1) {
+    return 1;
+  } else {
+    return testVariable + summation(testVariable - 1);
+  }
 }
 
-function gcd(testVariable1, testVariable2){
-    if(testVariable1 == testVariable2){
-        return testVariable1
-    } 
-    
-    if (testVariable1 > testVariable2){
-        return gcd(testVariable1 - testVariable2, testVariable2)
-    } else {
-        return gcd(testVariable2 - testVariable1, testVariable1)
-    }
+function gcd(testVariable1, testVariable2) {
+  if (testVariable1 == testVariable2) {
+    return testVariable1;
+  }
+
+  if (testVariable1 > testVariable2) {
+    return gcd(testVariable1 - testVariable2, testVariable2);
+  } else {
+    return gcd(testVariable2 - testVariable1, testVariable1);
+  }
 }
 
-console.log(gcd(6,9));
+console.log(gcd(6, 9));
