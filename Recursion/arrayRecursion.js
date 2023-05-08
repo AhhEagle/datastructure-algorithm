a9ddd0687fdb3c0ded13121b8a85c5f9984fd4c0
AhhEagle <aliyu.olateju@gmail.com>
2023-05-08 18:20:23 +0100

    modify:update array recursion

--- Recursion/arrayRecursion.js
+++ Recursion/arrayRecursion.js
@@ -1,70 +1,73 @@
 function count(myArray, key) {
-    // Base case
-    if (myArray.length == 0) {
-      return 0;
-    }
-    if (myArray[0] == key) {
-      return 1 + count(myArray.slice(1), key);
-    }
-      return 0 + count(myArray.slice(1), key);
-    }
-  function reverseArray(myArray){
-    if (myArray.length == 0) {
-        return [];
-      }
-      
-      else if (myArray.length == 1) {
-       return myArray;
-      }
-    
-      return [myArray[myArray.length - 1]].concat(reverseArray(myArray.slice(0, myArray.length - 1)));
+  // Base case
+  if (myArray.length == 0) {
+    return 0;
+  }
+  if (myArray[0] == key) {
+    return 1 + count(myArray.slice(1), key);
+  }
+  return 0 + count(myArray.slice(1), key);
+}
+function reverseArray(myArray) {
+  if (myArray.length == 0) {
+    return [];
+  } else if (myArray.length == 1) {
+    return myArray;
   }
 
-  function replaceNegative(array, currentIndex) {
-    if (currentIndex < array.length) {
-      if (array[currentIndex] < 0) {
-        array[currentIndex] = 0;
-      }
-  
-      replaceNegative(array, currentIndex + 1);
+  return [myArray[myArray.length - 1]].concat(
+    reverseArray(myArray.slice(0, myArray.length - 1))
+  );
+}
+
+function replaceNegative(array, currentIndex) {
+  if (currentIndex < array.length) {
+    if (array[currentIndex] < 0) {
+      array[currentIndex] = 0;
     }
-    return;
+
+    replaceNegative(array, currentIndex + 1);
   }
-  
-  function average(testVariable, currentIndex = 0) {
-	if (currentIndex == testVariable.length - 1) { 
-		return testVariable[currentIndex]
-	}
-	
-	if (currentIndex == 0) { 
-		return ((testVariable[currentIndex] + average(testVariable, currentIndex + 1)) / testVariable.length)
-	}
-	
-	// Compute sum 
-	return (testVariable[currentIndex] + average(testVariable, currentIndex + 1)) 
+  return;
+}
+
+function average(testVariable, currentIndex = 0) {
+  if (currentIndex == testVariable.length - 1) {
+    return testVariable[currentIndex];
+  }
+
+  if (currentIndex == 0) {
+    return (
+      (testVariable[currentIndex] + average(testVariable, currentIndex + 1)) /
+      testVariable.length
+    );
+  }
+
+  // Compute sum
+  return testVariable[currentIndex] + average(testVariable, currentIndex + 1);
 }
 
 function sort(testVariable, length) {
-	// Base case
-	if (length <= 1) {
-		return
-  }
-	
-	// Recursive case
-	// Call sort() again for first n - 1 elements
-	sort(testVariable, length - 1)
-
-	// Insert last element at its correct position in sorted array
-	var lastElement = testVariable[length - 1] // fetch the last element
-	var temp = length - 2 // start finding its correct location from one element before it
-	
-	// Move elements of testVariable[0..i-1], that are greater than key, to one position ahead of their current position 
-	while (temp >= 0 && testVariable[temp] > lastElement) {
-		testVariable[temp + 1] = testVariable[temp]
-		temp = temp - 1
+  // Base case
+  if (length <= 1) {
+    return;
   }
 
-	testVariable[temp + 1] = lastElement // place the element in its correct position
+  // Recursive case
+  // Call sort() again for first n - 1 elements
+  sort(testVariable, length - 1);
+
+  // Insert last element at its correct position in sorted array
+  var lastElement = testVariable[length - 1]; // fetch the last element
+  var temp = length - 2; // start finding its correct location from one element before it
+
+  // Move elements of testVariable[0..i-1], that are greater than key, to one position ahead of their current position
+  while (temp >= 0 && testVariable[temp] > lastElement) {
+    testVariable[temp + 1] = testVariable[temp];
+    temp = temp - 1;
+  }
+
+  testVariable[temp + 1] = lastElement; // place the element in its correct position
 }
 
 //flatten an array
@@ -72,19 +75,19 @@ function sort(testVariable, length) {
 function flatten(nestedArray) {
   const newArray = [];
 
-  for(let i = 0; i < nestedArray.length; i++) {
-      const thisItem = nestedArray[i];
-    
-       if(Array.isArray(thisItem)) {
-           const flatItem = flatten(thisItem);
-         
-           for(let j = 0; j < flatItem.length; j++) {
-               newArray.push(flatItem[j]);
-           }
-       } else {
-           newArray.push(thisItem);
-       }
+  for (let i = 0; i < nestedArray.length; i++) {
+    const thisItem = nestedArray[i];
+
+    if (Array.isArray(thisItem)) {
+      const flatItem = flatten(thisItem);
+
+      for (let j = 0; j < flatItem.length; j++) {
+        newArray.push(flatItem[j]);
+      }
+    } else {
+      newArray.push(thisItem);
+    }
   }
-  
+
   return newArray;
 }
