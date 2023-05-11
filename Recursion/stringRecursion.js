fe644551954b9e3258be2678bac98e4e61f25f59
AhhEagle <aliyu.olateju@gmail.com>
2023-05-11 23:47:43 +0100

    Modify:string recursion

--- Recursion/stringRecursion.js
+++ Recursion/stringRecursion.js
@@ -1,57 +1,46 @@
 function removeSpace(string) {
-    // Base case
-    if (string.length == 0) {
-        return "";
-    }
-  
-    if (string[0] == "\t" || string[0] == " ") {
-        return removeSpace(string.substr(1));
-    }
-    else {
-        return string[0] + removeSpace(string.substr(1));
-    }
+  // Base case
+  if (string.length == 0) {
+    return "";
   }
 
-  function removeAdjDuplicates(string) {
-    // Base case
-    if (string.length <= 1) {
-        return string;
-    }
-  
-    else if (string[0] == string[1]) {
-        return removeAdjDuplicates(string.substr(1));
-    }
-  
-    return string[0] + removeAdjDuplicates(string.substr(1));
+  if (string[0] == "\t" || string[0] == " ") {
+    return removeSpace(string.substr(1));
+  } else {
+    return string[0] + removeSpace(string.substr(1));
   }
-  
-  function mergeLexico(string1, string2) {
-    if (string1.length == 0) {
-      if (string2.length == 0) { 
-        return "";
-      }
-      return string2;
-    }
-  
-    else if (string2.length == 0) {
-      return string1;
-    }
+}
 
-    else if (string1[0] > string2[0]) {
-      return string2[0] + mergeLexico(string1, string2.substr(1));
-    }
-  
-    else {
-      return string1[0] + mergeLexico(string1.substr(1), string2);
-    }
+function removeAdjDuplicates(string) {
+  // Base case
+  if (string.length <= 1) {
+    return string;
+  } else if (string[0] == string[1]) {
+    return removeAdjDuplicates(string.substr(1));
   }
 
-  function sumDigits(testVariable) {
-    if (testVariable === "") {
-      return 0;
+  return string[0] + removeAdjDuplicates(string.substr(1));
+}
+
+function mergeLexico(string1, string2) {
+  if (string1.length == 0) {
+    if (string2.length == 0) {
+      return "";
     }
-    else {
-      return Number(testVariable[0]) + sumDigits(testVariable.substr(1));
-    }
+    return string2;
+  } else if (string2.length == 0) {
+    return string1;
+  } else if (string1[0] > string2[0]) {
+    return string2[0] + mergeLexico(string1, string2.substr(1));
+  } else {
+    return string1[0] + mergeLexico(string1.substr(1), string2);
   }
-  
+}
+
+function sumDigits(testVariable) {
+  if (testVariable === "") {
+    return 0;
+  } else {
+    return Number(testVariable[0]) + sumDigits(testVariable.substr(1));
+  }
+}
