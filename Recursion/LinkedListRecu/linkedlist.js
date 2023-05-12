36fb16e274bfef49a4c1ea5f2b420992e03a4903
AhhEagle <aliyu.olateju@gmail.com>
2023-05-12 21:48:35 +0100

    Modify linked list

--- Recursion/LinkedListRecu/linkedlist.js
+++ Recursion/LinkedListRecu/linkedlist.js
@@ -1,35 +1,34 @@
-import Node from "./node.js"
+import Node from "./node.js";
 export default class LinkedList {
-	constructor() {
-		this.head = null;
-	}
-	
-	appendNode(newData) {
-		let newNode = new Node(newData);
-		if (this.head == null) {
-			this.head = newNode;
-			return;
-		}
-		
-		let last = this.head;
-		while (last.next != null) {
-			last = last.next;
-		}
-		last.next =  newNode;
-	}
-	
-	//function for printing the linked list
-	printList() {
-		let temp = this.head;
-		while(temp != null) {
-			temp = temp.next;
-		}
-	}
+  constructor() {
+    this.head = null;
+  }
+
+  appendNode(newData) {
+    let newNode = new Node(newData);
+    if (this.head == null) {
+      this.head = newNode;
+      return;
+    }
+
+    let last = this.head;
+    while (last.next != null) {
+      last = last.next;
+    }
+    last.next = newNode;
+  }
+
+  //function for printing the linked list
+  printList() {
+    let temp = this.head;
+    while (temp != null) {
+      temp = temp.next;
+    }
+  }
 }
 
-
-let test =  new LinkedList();
-test.appendNode('6');
-test.appendNode('8');
-test.appendNode('9');
+let test = new LinkedList();
+test.appendNode("6");
+test.appendNode("8");
+test.appendNode("9");
 test.printList();
