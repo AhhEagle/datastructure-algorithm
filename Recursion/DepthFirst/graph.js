4c3d6e502ef6176d4e6d3666cfed4d1a7e49d9f8
AhhEagle <aliyu.olateju@gmail.com>
2023-05-12 21:48:02 +0100

    Modify graph

--- Recursion/DepthFirst/graph.js
+++ Recursion/DepthFirst/graph.js
@@ -1,20 +1,15 @@
 export default class Graph {
-    constructor(vertices = 0) {
-      this.graph = new Map();
-      this.vertices = vertices;
-    }
-      
-    addEdge(u, v) {
-      if(this.graph.has(u))
-      {
-        this.graph.get(u).push(v); 
-      }
-      else
-      {
-        this.graph.set(u, []);
-        this.graph.get(u).push(v); 
-      }
+  constructor(vertices = 0) {
+    this.graph = new Map();
+    this.vertices = vertices;
+  }
+
+  addEdge(u, v) {
+    if (this.graph.has(u)) {
+      this.graph.get(u).push(v);
+    } else {
+      this.graph.set(u, []);
+      this.graph.get(u).push(v);
     }
   }
-  
- 
+}
