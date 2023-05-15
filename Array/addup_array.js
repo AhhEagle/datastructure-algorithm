537dcdf445ea02f50f8f5ad8d143a6b16c157217
AhhEagle <aliyu.olateju@gmail.com>
2023-05-15 22:51:36 +0100

    Modify array addup

--- Array/addup_array.js
+++ Array/addup_array.js
@@ -1,7 +1,7 @@
-function sumUp(arr, val){
-    for(let i=0; i<arr.length; i++){
-        for(let j=i+1; j<arr.length; j++){
-            if(arr[i] + arr[j] == val){
+function sumUp(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] == val) {
                 return [arr[i], arr[j]]
             }
         }
@@ -10,41 +10,41 @@ function sumUp(arr, val){
 
 }
 
-function binarySearch(arr, item){
+function binarySearch(arr, item) {
     let first = 0;
     let mid;
     let last = arr.length - 1;
     let arrayIndex = -1
     let seen = false;
-    while ((first <= last) && !seen){
+    while ((first <= last) && !seen) {
         console.log(!seen)
-        mid = Math.floor((first + last)/2);
-        if( arr[mid] == item){
+        mid = Math.floor((first + last) / 2);
+        if (arr[mid] == item) {
             arrayIndex = mid
             seen = true
         }
-        else{
-            if(item < arr[mid])
+        else {
+            if (item < arr[mid])
                 last = mid - 1
             else
                 first = mid + 1
         }
     }
     if (seen)
-    return arrayIndex
-else
-    return false
+        return arrayIndex
+    else
+        return false
 }
-function anotherSumUp(arr, val){
-    arr.sort((a,b)=>{return a-b});
+function anotherSumUp(arr, val) {
+    arr.sort((a, b) => { return a - b });
     let index;
-    for(let i =0; i<arr.length; i++){
-        index =  binarySearch(arr, val-arr[i])
-        if(index != false && i!=index){
-            return [arr[i], val-arr[i]]
+    for (let i = 0; i < arr.length; i++) {
+        index = binarySearch(arr, val - arr[i])
+        if (index != false && i != index) {
+            return [arr[i], val - arr[i]]
         }
     }
     return false;
 
 }
-console.log(anotherSumUp([2,4,6,8], 10))
+console.log(anotherSumUp([2, 4, 6, 8], 10))
