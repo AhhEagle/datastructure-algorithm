94b7454547c0f70a7cc1e985c7d2e4387ac5158a
AhhEagle <aliyu.olateju@gmail.com>
2023-05-15 22:52:33 +0100

    Modify test array

--- Array/addup_array.js
+++ Array/addup_array.js
@@ -1,50 +1,45 @@
 function sumUp(arr, val) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] == val) {
-                return [arr[i], arr[j]]
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == val) {
+        return [arr[i], arr[j]];
+      }
     }
-    return false;
-
+  }
+  return false;
 }
 
 function binarySearch(arr, item) {
-    let first = 0;
-    let mid;
-    let last = arr.length - 1;
-    let arrayIndex = -1
-    let seen = false;
-    while ((first <= last) && !seen) {
-        console.log(!seen)
-        mid = Math.floor((first + last) / 2);
-        if (arr[mid] == item) {
-            arrayIndex = mid
-            seen = true
-        }
-        else {
-            if (item < arr[mid])
-                last = mid - 1
-            else
-                first = mid + 1
-        }
+  let first = 0;
+  let mid;
+  let last = arr.length - 1;
+  let arrayIndex = -1;
+  let seen = false;
+  while (first <= last && !seen) {
+    console.log(!seen);
+    mid = Math.floor((first + last) / 2);
+    if (arr[mid] == item) {
+      arrayIndex = mid;
+      seen = true;
+    } else {
+      if (item < arr[mid]) last = mid - 1;
+      else first = mid + 1;
     }
-    if (seen)
-        return arrayIndex
-    else
-        return false
+  }
+  if (seen) return arrayIndex;
+  else return false;
 }
 function anotherSumUp(arr, val) {
-    arr.sort((a, b) => { return a - b });
-    let index;
-    for (let i = 0; i < arr.length; i++) {
-        index = binarySearch(arr, val - arr[i])
-        if (index != false && i != index) {
-            return [arr[i], val - arr[i]]
-        }
+  arr.sort((a, b) => {
+    return a - b;
+  });
+  let index;
+  for (let i = 0; i < arr.length; i++) {
+    index = binarySearch(arr, val - arr[i]);
+    if (index != false && i != index) {
+      return [arr[i], val - arr[i]];
     }
-    return false;
-
+  }
+  return false;
 }
-console.log(anotherSumUp([2, 4, 6, 8], 10))
+console.log(anotherSumUp([2, 4, 6, 8], 10));
--- Array/test.js
+++ Array/test.js
@@ -1,156 +1,150 @@
-function pali(strg){
-    let start = 0;
-    let end = strg.length - 1;
+function pali(strg) {
+  let start = 0;
+  let end = strg.length - 1;
 
-    while (start < end){
-        if(strg[start] != strg[end]){
-            return false
-        }
-        start ++;
-        end --;
+  while (start < end) {
+    if (strg[start] != strg[end]) {
+      return false;
     }
+    start++;
+    end--;
+  }
 
-    return true;
+  return true;
 }
 
+function target(arr, target) {
+  //array is sorted
+  //two pointers to move through the array
+  //add the numbers if its the target return the numbers
+  //if its greater than the target, decrease the end
+  //if its lesser than the target increase the start
+  let left = 0;
+  let right = arr.length - 1;
 
-function target(arr, target){
-    //array is sorted 
-    //two pointers to move through the array 
-    //add the numbers if its the target return the numbers 
-    //if its greater than the target, decrease the end
-    //if its lesser than the target increase the start
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left < right){
-        let curr = (arr[left] + arr[right]) ;
-        if(curr == target){
-            return [arr[left], arr[right]];
-        }
-
-        if(curr > target){
-            right --;
-        }
-
-        
-        if(curr < target){
-            left ++;
-        }
+  while (left < right) {
+    let curr = arr[left] + arr[right];
+    if (curr == target) {
+      return [arr[left], arr[right]];
     }
 
-    return false;
+    if (curr > target) {
+      right--;
+    }
+
+    if (curr < target) {
+      left++;
+    }
+  }
+
+  return false;
 }
 
+function sortTwoArrays(arr1, arr2) {
+  //[1,4,6] [2,3,5]
+  //[1,2,3,4,5]
 
+  let pointerone = 0;
+  let pointertwo = 0;
+  let result = [];
 
-function sortTwoArrays(arr1, arr2){
-    //[1,4,6] [2,3,5]
-    //[1,2,3,4,5]
-
-    let pointerone = 0;
-    let pointertwo = 0;
-    let result = [];
-
-    while((pointerone < arr1.length) && (pointertwo < arr2.length)){
-        if(arr1[pointerone] < arr2[pointertwo]){
-            result.push(arr1[pointerone]);
-            pointerone ++ ;
-        } else{
-            result.push(arr2[pointertwo]);
-            pointertwo++; 
-        }
+  while (pointerone < arr1.length && pointertwo < arr2.length) {
+    if (arr1[pointerone] < arr2[pointertwo]) {
+      result.push(arr1[pointerone]);
+      pointerone++;
+    } else {
+      result.push(arr2[pointertwo]);
+      pointertwo++;
     }
+  }
 
-    if(pointerone < arr1.length - 1){
-        arr1 = arr1.splice(0, pointerone);
-        result = result.concat(arr1);
-    }
+  if (pointerone < arr1.length - 1) {
+    arr1 = arr1.splice(0, pointerone);
+    result = result.concat(arr1);
+  }
 
-    if(pointertwo < arr2.length -1 ){
-        arr2 = arr2.splice(0, pointertwo);
-        result = result.concat(arr2);
-    }
+  if (pointertwo < arr2.length - 1) {
+    arr2 = arr2.splice(0, pointertwo);
+    result = result.concat(arr2);
+  }
 
-    return result;
-
+  return result;
 }
 
-
-
-function longestSum(nums, k){
-    //array of int [1,2,4,3,5]
-    //k =6;
-    let left = 0, curr = 0, ans = 0;
-    for (let right =0; right < nums.length; right++){
-        curr +=nums[right];
-        while(curr > k){
-            curr -=nums[left];
-            left ++;
-        }
-        ans = Math.max(ans, right - left + 1);
-
+function longestSum(nums, k) {
+  //array of int [1,2,4,3,5]
+  //k =6;
+  let left = 0,
+    curr = 0,
+    ans = 0;
+  for (let right = 0; right < nums.length; right++) {
+    curr += nums[right];
+    while (curr > k) {
+      curr -= nums[left];
+      left++;
     }
-    return ans;
+    ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
 }
 
-console.log(longestSum([1,2,4,3,5], 6));
+console.log(longestSum([1, 2, 4, 3, 5], 6));
 
-function longestSub(str){
-    //"1101100111"
-    //can only flip one zero 
-    let left = 0, curr = 0, ans = 0;
-    for (let right =0; right < str.length; right++){
-        if (str[right] == "0"){
-            curr ++;
-        }
+function longestSub(str) {
+  //"1101100111"
+  //can only flip one zero
+  let left = 0,
+    curr = 0,
+    ans = 0;
+  for (let right = 0; right < str.length; right++) {
+    if (str[right] == "0") {
+      curr++;
+    }
 
-        while(curr > 1){
-            if(str[left] == "0"){
-                curr -=1;
-            }
-            left++;
-        }
+    while (curr > 1) {
+      if (str[left] == "0") {
+        curr -= 1;
+      }
+      left++;
+    }
 
-        ans = Math.max(ans, right - left + 1);
-     
-    } 
-    return ans;   
+    ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
 }
 
 console.log(longestSub([1101100111]));
 
-function numSubarrayProductLessThanK(arr, k){
-    //[10, 5, 2, 6] k =100
-    if (k <=1){
-        return 0;
+function numSubarrayProductLessThanK(arr, k) {
+  //[10, 5, 2, 6] k =100
+  if (k <= 1) {
+    return 0;
+  }
+  let left = 0,
+    ans = 0;
+  curr = 1;
+  for (let right = 0; right < str.length; right++) {
+    curr *= arr[right];
+    while (left <= right && curr >= k) {
+      curr /= nums[left];
+      left++;
     }
-    let left = 0, ans = 0;
-    curr = 1;
-    for (let right =0; right < str.length; right++){
-        curr *= arr[right];
-        while (left <= right && curr >= k) {
-            curr /= nums[left];
-            left++;
-        }
-        
-        ans += right - left + 1;
-    } 
-    return ans 
-}
-function longestBestSubArr(nums, k){
-        let curr = 0;
-        for (let i = 0; i < k; i++) {
-            curr += nums[i];
-        }
-        
-        let ans = curr;
-        for (let i = k; i < nums.length; i++) {
-            curr += nums[i] - nums[i - k];
-            ans = Math.max(ans, curr);
-        }
-        
-        return ans;
-}
 
+    ans += right - left + 1;
+  }
+  return ans;
+}
+function longestBestSubArr(nums, k) {
+  let curr = 0;
+  for (let i = 0; i < k; i++) {
+    curr += nums[i];
+  }
 
+  let ans = curr;
+  for (let i = k; i < nums.length; i++) {
+    curr += nums[i] - nums[i - k];
+    ans = Math.max(ans, curr);
+  }
+
+  return ans;
+}
