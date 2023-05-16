109136f6d8ab2d693ae78f9300029dd71d5da602
AhhEagle <aliyu.olateju@gmail.com>
2023-05-16 23:02:18 +0100

    Modify set

--- Set/set.js
+++ Set/set.js
@@ -1,56 +1,64 @@
-function mySet(){
-let collection = [];
-this.has =  function(element){
-    return(collection.indexOf(element) !== -1);
-};
-this.values = function(){
+function mySet() {
+  let collection = [];
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+  this.values = function () {
     return collection;
-};
+  };
 
-this.add= function(element){
-    if(!this.has(element)){
-        collection.push(element);
-        return true;
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
     }
     return false;
-};
+  };
 
-this.remove = function(element){
-    if(this.has(element)){
-        let index =  collection.indexOf(element);
-        collection.splice(index,1);
-        return true;
+  this.remove = function (element) {
+    if (this.has(element)) {
+      let index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
     }
     return false;
-}
-this.size = function(){
+  };
+  this.size = function () {
     return collection.length;
-}
-this.union = function(otherSet){
+  };
+  this.union = function (otherSet) {
     let unionSet = new mySet();
     let firstSet = this.values();
     let secondSet = otherSet.values();
-    firstSet.forEach((e)=> unionSet.add(e));
-    secondSet.forEach((e)=> unionSet.add(e));
+    firstSet.forEach((e) => unionSet.add(e));
+    secondSet.forEach((e) => unionSet.add(e));
     return unionSet;
-}
-this.intersection = function(otherSet){
+  };
+  this.intersection = function (otherSet) {
     let intersectionSet = new mySet();
     let firstSet = this.values();
-    firstSet.forEach((e)=> {if(otherSet.has(e)){intersectionSet.add(e)}});
+    firstSet.forEach((e) => {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
     return intersectionSet;
-}
+  };
 
-this.difference = (otherSet)=>{
-  let differenceSet = new mySet();
-  let firstSet = this.values();
-  firstSet.forEach((e)=>{if(!otherSet.has(e)) differenceSet.add(e)});
-  return differenceSet;
-};
-this.subset = (otherSet)=>{
+  this.difference = (otherSet) => {
+    let differenceSet = new mySet();
     let firstSet = this.values();
-    return firstSet.every((value)=>{ return otherSet.has(value)});
-}
+    firstSet.forEach((e) => {
+      if (!otherSet.has(e)) differenceSet.add(e);
+    });
+    return differenceSet;
+  };
+  this.subset = (otherSet) => {
+    let firstSet = this.values();
+    return firstSet.every((value) => {
+      return otherSet.has(value);
+    });
+  };
 }
 
 let setOne = new mySet();
@@ -62,5 +70,3 @@ setTwo.add("a");
 setTwo.add("d");
 //console.log(setOne.subset(setTwo));
 //console.log(setOne.intersection(setTwo).values());
-
-
