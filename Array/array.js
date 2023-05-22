fdf157b4e621ff50840e999a45029eff2837d992
AhhEagle <aliyu.olateju@gmail.com>
2023-05-22 23:36:44 +0100

    remove console logs in arrays

--- Array/array.js
+++ Array/array.js
@@ -1,117 +1,110 @@
-class Array{
-    constructor(){
-        this.list = [];
+class Array {
+  constructor() {
+    this.list = [];
+  }
+  insert(value) {
+    this.list.push(value);
+  }
+  removeAt(index) {
+    if (index > this.list.length) {
+      return -1;
     }
-    insert(value){
-     this.list.push(value);
-    }
-    removeAt(index){
-        if (index > this.list.length){
-            return -1
-        }
     this.list[index] = 0;
     return this.list;
-    }
+  }
 }
 
 const permute = (nums) => {
-    const res = [];
-  
-    const go = (cur, rest) => {
-      if (rest.length === 0) {
-        res.push(cur);
-        return;
-      }
-  
-      for (let i = 0; i < rest.length; i++) {
-        // note if using array push and splice here, it will cause mutation
-        go(
-          [...cur, rest[i]],
-          [...rest.slice(0, i), ...rest.slice(i + 1)],
-        );
-      }
-    };
-  
-    go([], nums);
-    return res;
+  const res = [];
+
+  const go = (cur, rest) => {
+    if (rest.length === 0) {
+      res.push(cur);
+      return;
+    }
+
+    for (let i = 0; i < rest.length; i++) {
+      // note if using array push and splice here, it will cause mutation
+      go([...cur, rest[i]], [...rest.slice(0, i), ...rest.slice(i + 1)]);
+    }
   };
 
-  const newPermute = (nums) =>{
-      let result = [];
-      let temp = [];
+  go([], nums);
+  return res;
+};
 
-      findPermutation(temp, nums, result);
-      return result;
+const newPermute = (nums) => {
+  let result = [];
+  let temp = [];
+
+  findPermutation(temp, nums, result);
+  return result;
+};
+
+let findPermutation = (temp, nums, result) => {
+  if (!nums.length) {
+    result.push(temp.concat());
+    return;
   }
 
-  let findPermutation = (temp, nums, result)=>{
-        if(!nums.length){
-             result.push(temp.concat());
-             return;
-        }
+  for (let i = 0; i < nums.length; i++) {
+    const newNumber = nums[i];
+    temp.push(newNumber);
+    nums.splice(i, 1);
+    findPermutation(temp, nums, result);
+    console.log(temp + " nums: " + nums + " result: " + result);
+    let popped = temp.pop();
+    console.log("This was popped: " + popped);
+    nums.splice(i, 0, newNumber);
+    console.log("new number: " + nums + " " + i);
+  }
+};
 
-        for(let i =0; i<nums.length; i++){
-           console.log("maybe this will better explain it: " + nums);
-           console.log(i + " " + nums[i] );
-            const newNumber = nums[i];
-            temp.push(newNumber);
-            nums.splice(i, 1);
-          findPermutation(temp, nums, result);
-           console.log(temp + " nums: " + nums + " result: " + result);
-           let popped = temp.pop();
-         console.log("THis was popped: " + popped);
-           nums.splice(i, 0, newNumber);
-          console.log("new number: " + nums + " " + i);
-          
-        }
+function arraySubset(arr, sub) {
+  if (sub.length > arr.length) {
+    return false;
   }
 
-  function arraySubset(arr, sub) {
-    if(sub.length > arr.length) {
-        return false;
+  const arrMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (arrMap.has(item)) {
+      arrMap.set(item, arrMap.get(item) + 1);
+    } else {
+      arrMap.set(item, 1);
     }
-  
-    const arrMap = new Map();
-  
-    for(let i = 0; i < arr.length; i++) {
-        const item = arr[i];
-        if(arrMap.has(item)) {
-            arrMap.set(item, arrMap.get(item) + 1);
-        } else {
-            arrMap.set(item, 1);
-        }
+  }
+
+  for (let i = 0; i < sub.length; i++) {
+    const currentItem = sub[i];
+    if (!arrMap.has(currentItem)) {
+      return false;
     }
-    
-    for(let i = 0; i < sub.length; i++) {
-        const currentItem = sub[i];
-        if(!arrMap.has(currentItem)) {
-            return false;
-        }
-        
-        arrMap.set(currentItem, arrMap.get(currentItem) - 1);
-        if(arrMap.get(currentItem) === 0) {
-            arrMap.delete(currentItem);
-        }
+
+    arrMap.set(currentItem, arrMap.get(currentItem) - 1);
+    if (arrMap.get(currentItem) === 0) {
+      arrMap.delete(currentItem);
     }
-    
-    return true;
+  }
+
+  return true;
 }
 
 function getMaxProfit(prices) {
   let minPrice = Infinity;
   let maxProfit = 0;
 
-  for(let i = 0; i < prices.length; i++) {
-      const currentPrice = prices[i];
-      minPrice = Math.min(minPrice, currentPrice);
-      maxProfit = Math.max(maxProfit, currentPrice - minPrice);
+  for (let i = 0; i < prices.length; i++) {
+    const currentPrice = prices[i];
+    minPrice = Math.min(minPrice, currentPrice);
+    maxProfit = Math.max(maxProfit, currentPrice - minPrice);
   }
-  
+
   return maxProfit;
 }
 
 //newPermute([1,2,3]);
-
 
 // let test = new Array();
 // test.insert(6);
