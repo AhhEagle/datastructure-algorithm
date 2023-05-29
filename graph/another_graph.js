05283d9dde0fb61b82031ce204ad8bb54a788021
AhhEagle <aliyu.olateju@gmail.com>
2023-05-29 22:59:12 +0100

    modify another graph

--- graph/another_graph.js
+++ graph/another_graph.js
@@ -1,20 +1,20 @@
 class Graph {
- constructor(){
-     this.noOfVertices = 0;
-     this.adjList = new Map();
- }
+  constructor() {
+    this.noOfVertices = 0;
+    this.adjList = new Map();
+  }
 
- addVertex(vertex){
-     this.adjList.set(vertex, []);
-     this.noOfVertices++;
- }
+  addVertex(vertex) {
+    this.adjList.set(vertex, []);
+    this.noOfVertices++;
+  }
 
- addEdge(vertex1, vertex2){
-     this.adjList.get(vertex1).push(vertex2);
-     this.adjList.get(vertex2).push(vertex1);
- }
+  addEdge(vertex1, vertex2) {
+    this.adjList.get(vertex1).push(vertex2);
+    this.adjList.get(vertex2).push(vertex1);
+  }
 
- print() {
+  print() {
     const keys = this.adjList.keys();
     for (let i of keys) {
       const values = this.adjList.get(i);
@@ -31,7 +31,7 @@ let graph = new Graph();
 graph.addVertex("a");
 graph.addVertex("b");
 graph.addVertex("c");
-graph.addEdge("a","b");
-graph.addEdge("a","c");
+graph.addEdge("a", "b");
+graph.addEdge("a", "c");
 
 console.log(graph.print());
