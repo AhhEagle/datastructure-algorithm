7668fa54e7804509b4114c7a44a8fb1ff28137af
AhhEagle <aliyu.olateju@gmail.com>
2023-05-31 22:24:10 +0100

    modify test

--- test.js
+++ test.js
@@ -1,49 +1,44 @@
-class Node{
-    constructor(data){
-        this.data = data;
-        this.next = null
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-class SlinkedList{
-    
-    constructor(){
-        this.head = null;   
+class SlinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addElement(element) {
+    let node = new Node(element);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      console.log(node.next);
+      this.head = node;
     }
+  }
 
-    addElement(element){
-        let node = new Node(element);
-        if(this.head == null){
-            this.head = node;
-        } else {
-            node.next = this.head;
-            console.log(node.next)
-            this.head = node;
-        }
+  reverse() {
+    let pre = this.head;
+    let current = this.head.next;
+    pre.next = null;
+    while (current !== null) {
+      let newP = current;
+      current = current.next;
+
+      newP.next = pre;
+      pre = newP;
     }
-
-    reverse(){
-        let pre = this.head;
-        let current = this.head.next
-        pre.next = null;
-        while (current !== null){
-            let newP = current;
-            current = current.next;
-
-            newP.next = pre;
-            pre = newP;
-
-        }
-        this.head = pre
-    }
-
+    this.head = pre;
+  }
 }
-
-
 
 let testone = new SlinkedList();
 testone.addElement(5);
 testone.addElement(6);
 testone.addElement(7);
 testone.reverse();
-console.log(testone)
+console.log(testone);
