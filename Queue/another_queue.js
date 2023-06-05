728917b3b1fbb49a04323d332028c0c8ee01cd88
AhhEagle <aliyu.olateju@gmail.com>
2023-06-05 21:11:34 +0100

    Format queue

--- Queue/another_queue.js
+++ Queue/another_queue.js
@@ -1,19 +1,17 @@
 //generate binary numbers from 1 till n
 function findBin(n) {
-    let result = [];
-    let myQueue = new Queue();
-    var s1, s2;
-    myQueue.enqueue("1");
-    for (var i = 0; i < n; i++) {
+  let result = [];
+  let myQueue = new Queue();
+  var s1, s2;
+  myQueue.enqueue("1");
+  for (var i = 0; i < n; i++) {
+    result.push(myQueue.dequeue());
+    s1 = result[i] + "0";
+    s2 = result[i] + "1";
 
-        result.push(myQueue.dequeue());
-        s1 = result[i] + "0";
-        s2 = result[i] + "1";
+    myQueue.enqueue(s1);
+    myQueue.enqueue(s2);
+  }
 
-        myQueue.enqueue(s1);
-        myQueue.enqueue(s2);
-
-    }
-
-    return result;
+  return result;
 }
